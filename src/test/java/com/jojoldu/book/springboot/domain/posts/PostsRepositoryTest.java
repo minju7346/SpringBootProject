db69b6b6e975a2315eb3e6c51de08a2841c31e5a
minju7346 <minju7346@naver.com>
2022-02-04 15:35:47 +0900

    Chapter3

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 논문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("minju@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
