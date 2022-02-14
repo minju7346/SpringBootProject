270c5523e46febebaf0a7aefa7b50937307cb97c
minju7346 <minju7346@naver.com>
2022-02-14 18:02:32 +0900

    Refactoring

---
+++ src/test/java/com/minju/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.minju.study.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 논문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("minju@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now = LocalDateTime.of(2022,2,8,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>creteDate="+posts.getCretedDate()+",modifiedDate="+posts.getModifiedDate());
+        assertThat(posts.getCretedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
---
+++ src/test/java/com/minju/study/springboot/web/dto/HelloReponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.minju.study.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloReponseDtoTest {
+
+    @Test
+    public void 룸복_기능_테스트(){
+        String name = "test";
+        int amout = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name,amout);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amout);
+    }
+}
